b65bb05e724078418a752502b9eaf1038ac30811
Dhairya Parikh <dhairyaparikh769@gmail.com>
2022-03-12 12:13:59 +0530

    Offer Rides Model

---
+++ app/models/offerRides.model.js
@@ -0,0 +1,78 @@
+//DB_URL=mongodb+srv://Darshan:Darshan1310@cluster0.94usl.gcp.mongodb.net/SE-BACKEND
+
+const mongo = require('mongoose');
+const offer_rides = new mongo.Schema({
+    current_location: {
+        type : {
+            lat : {
+                type : String,
+            },
+            lng : {
+                type : String
+            }
+        },
+        required : true
+    },
+
+    starting_location: {
+        type : {
+            lat : {
+                type : String,
+            },
+            lng : {
+                type : String
+            }
+        },
+        required : true
+    },
+
+    starting_address_line1: {
+        type: String,
+        required: true,
+    },
+
+    starting_address_line2: {
+        type: String,
+        required: true,
+    },
+
+    starting_city: {
+        type: String,
+        required: true,
+    },
+
+    starting_state: {
+        type: String,
+        required: true,
+    },
+
+    ending_location: {
+        type : {
+            lat : {
+                type : String,
+            },
+            lng : {
+                type : String
+            }
+        },
+        required : true
+    },
+
+    end_address_line1: {
+        type: String,
+        required: true,
+    },
+
+    end_city: {
+        type: String,
+        required: true,
+    },
+
+    end_state: {
+        type: String,
+        required: true,
+
+    }
+})
+
+module.exports = mongoose.model("offer_rides", userSchema);
