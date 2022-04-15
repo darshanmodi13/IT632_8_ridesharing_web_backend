7a2749c4215ce7df98e0fded172780239d79c571
mitali <202112070@daiict.ac.in>
2022-04-15 18:32:14 +0530

    socket connection

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var envirornment = process.env.NODE_ENV || "development";
 if (envirornment === "development") {
   require("dotenv").config();
 }
+//require socket
+require("./socket");
 
 //packages
 const express = require("express");
@@ -10,6 +12,9 @@ const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const path = require("path");
 const bodyParser = require("body-parser");
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
 
 //swagger
 const swaggerDoc = require("./app/config/swagger.config");
